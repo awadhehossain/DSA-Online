57896ba176982e9a8b2e052d870836c4c22f75a2
awadhehossain <awadhehossain67@gmail.com>
2025-07-27 12:22:15 +0600

    Bubble Sort

---
+++ Bubble sort/C/All need function/ascending_order.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+
+void printarray(int arr[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+       printf("%d ",arr[i]);
+    }
+}
+
+void swap(int *a,int *b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void bubble_sort(int arr[],int n)
+{
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                swap(&arr[j],&arr[j+1]);
+            }
+            
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    int arr [n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    bubble_sort(arr,n);
+    printarray(arr,n);
+
+    return 0;
+
+}
---
+++ Bubble sort/Python/ascending.py
@@ -0,0 +1,27 @@
+def bubble_sort(nums) : #bubble sort Method
+
+    for i in range(0,len(nums)-1):
+        for j in range(len(nums)-i-1):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j] #Swaping
+    return nums #returning the list 
+
+#Taking input for list size
+n=int(input("Enter the size of the list:"))
+nums=[] #Decleare the list 
+print(f"Enter {n} numbers:")# f string
+#Taking list input 
+for _ in range(n):#_ don't care variable
+    num=int(input())
+    nums.append(num) #In append mode take one input and add it one by one to the list
+
+sorted_nums = bubble_sort(nums)#Method call
+print("Sorted list:", sorted_nums)#Printing the sorted list
+
+
+
+   
+
+
+
+
