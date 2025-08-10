dca18eb92e83914a04002d20981eb63081335815
awadhehossain <awadhehossain67@gmail.com>
2025-08-10 10:49:37 +0600

    Sheel sort

---
+++ Shell Sort/C/descending.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+void swap(int *x,int *y)
+{
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+
+void shell_sort(int arr[],int n)
+{
+    for(int gap=n/2;gap>=1;gap=gap/2)
+    {
+        for(int j=gap;j<=n-1;j++)
+        {
+            for(int i=j-gap;i>=0;i=i-gap)
+            {
+                if(arr[i+gap] < arr[i])
+                {
+                    break;
+                }
+                else
+                {
+                    swap(&arr[i+gap],&arr[i]);
+                }
+            }
+        }
+    }
+}
+
+int main ()
+{
+    int n;
+    printf("Enter the array size:");
+    scanf("%d",&n);
+    int arr[n];
+
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    shell_sort(arr,n);
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
